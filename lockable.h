911832c3c45f34c82aeb69415b39443c4f71c5f3
Sagitrs <sagitrs67@gmail.com>
2022-04-28 13:21:24 +0800

    Update Lock.

--- lockable.h
+++ lockable.h
@@ -1,21 +1,44 @@
 #pragma once
 #include <mutex>
+#include <shared_mutex>
+//#define LOCK_TYPE_MUTEX
+#define LOCK_TYPE_PTHREAD_RWLOCK
+//#define LOCK_TYPE_SHARED_MUTEX
 
 namespace sagitrs {
 
 struct Lockable {
+#if defined(LOCK_TYPE_MUTEX)
+ private:
+  std::mutex mu_;
+ public:
   Lockable() = default;
   virtual ~Lockable() {}
-
-  Lockable(const Lockable&) = delete;
-  Lockable& operator=(const Lockable&) = delete;
-
+  void ReadLock() { mu_.lock(); }
+  void WriteLock() { mu_.lock(); }
+  void Unlock() { mu_.unlock(); }
+  void AssertHeld() {}
+#elif defined(LOCK_TYPE_PTHREAD_RWLOCK)
+ private:
+  pthread_rwlock_t lock_;
+ public:
+  Lockable() : lock_() { pthread_rwlock_init(&lock_, nullptr); }
+  virtual ~Lockable() { pthread_rwlock_destroy(&lock_); }
+  void ReadLock() { pthread_rwlock_rdlock(&lock_); }
+  void WriteLock() { pthread_rwlock_wrlock(&lock_); }
+  void Unlock() { pthread_rwlock_unlock(&lock_); }
+  void AssertHeld() {}
+#elif defined(LOCK_TYPE_SHARED_MUTEX)
+#else
   void ReadLock() { mu_.lock(); }
   void WriteLock() { mu_.lock(); }
   void Unlock() { mu_.unlock(); }
   void AssertHeld() {}
  private:
   std::mutex mu_;
+#endif
+  Lockable(const Lockable&) = delete;
+  Lockable& operator=(const Lockable&) = delete;
 };
 
 struct LockGuard {
