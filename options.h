4fb50f19d381f28ae92cae64273214879f429be9
Sagitrs <sagitrs67@gmail.com>
2022-05-14 23:43:07 +0800

    scorer & compaction.

--- options.h
+++ options.h
@@ -136,7 +136,7 @@ struct SBSOptions : public SBSNodeOptions, public StatisticsOptions {
   double SpaceAmplificationConst() const { return 0.3; }
   double CacheCapacity() const { return 0.3; }
   double ApproximateBufferNodeConst() const { return 1.0 / (DefaultWidth() - 1); }
-  double FilesPerNode() const { return SpaceAmplificationConst() / ApproximateBufferNodeConst(); }
+  //double FilesPerNode() const { return SpaceAmplificationConst() / ApproximateBufferNodeConst(); }
   double LevelCapabilityConst(size_t level) const { return std::pow(1.0 / DefaultWidth(), level); }
  
  public:
